72ff06931e5a0d1f31b4a4d2dccee42d7de7a7d3
minniejung <jungmignonne@gmail.com>
2025-02-21 13:46:53 +0900

    feat: validatior functions

--- src/script.js
+++ src/script.js
@@ -1,9 +1,31 @@
 // TODO : DOM으로부터 필요한 엘리먼트를 불러오세요.
+let elInputUsername = document.querySelector("#username");
+let elFailureMessage = document.querySelector(".failure-message");
+let elSuccessMessage = document.querySelector(".success-message");
+
+let inputPassword = document.querySelector("#password");
+let inputPasswordRetype = document.querySelector("#password-retype");
+let passwordMismatchMsg = document.querySelector(".mismatch-message");
+
+// VALIDATION CHECK - ID
+elInputUsername.onkeyup = function () {
+  let isValid = isMoreThan4Length(elInputUsername.value);
+
+  elSuccessMessage.classList.toggle("hide", !isValid);
+  elFailureMessage.classList.toggle("hide", isValid);
+};
 
 function isMoreThan4Length(value) {
-  // TODO : 이 함수를 완성하세요.
+  return value.length >= 4;
 }
 
+// VALIDATION CHECK - PASSWORD
+inputPasswordRetype.onkeyup = function () {
+  let isMatching = isMatch(inputPassword.value, inputPasswordRetype.value);
+
+  passwordMismatchMsg.classList.toggle("hide", isMatching);
+};
+
 function isMatch(password1, password2) {
-  // TODO : 이 함수를 완성하세요.
+  return password1 === password2;
 }
